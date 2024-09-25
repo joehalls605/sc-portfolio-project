8298b07ff8569f560824c6edc0ac8151607d62ea
Joeh <joehalls605@gmail.com>
2024-09-25 09:02:29 +0100

    line-effect removed, working ctas

--- wp-content/themes/josephhalls/header.php
+++ wp-content/themes/josephhalls/header.php
@@ -8,7 +8,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-    <script src="<?php echo get_template_directory_uri();?>/assets/js/scripts.js"></script>
 <body <?php body_class();?>>
 <header class="site-header">
     <div class="container">
